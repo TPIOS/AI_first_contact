76b8167e53e4caa2a762663c67e75ec4f17e99c0
TPIOS <116010062@link.cuhk.edu.cn>
2019-10-15 16:32:23 +0800

    94.19% already

--- NLP/POS-tagging/runtagger.py
+++ NLP/POS-tagging/runtagger.py
@@ -2,6 +2,8 @@ import os
 import math
 import sys
 import datetime
+from copy import deepcopy
+import numpy as np
 
 def loadModel(model_file):
     modelFile = open(model_file, "r")
@@ -24,99 +26,75 @@ def loadModel(model_file):
             continue
         firstKey, secondKey = manyKeys.split(" ")
         if whichDict == 1:
-            tagBeforeTag[firstKey] = dict()
-            tagBeforeTag[firstKey][secondKey] = times
+            if firstKey in tagBeforeTag:
+                tagBeforeTag[firstKey][secondKey] = times
+            else:
+                tagBeforeTag[firstKey] = dict()
+                tagBeforeTag[firstKey][secondKey] = times
         if whichDict == 2:
-            tagAfterTag[firstKey] = dict()
-            tagAfterTag[firstKey][secondKey] = times
+            if firstKey in tagAfterTag:
+                tagAfterTag[firstKey][secondKey] = times
+            else:
+                tagAfterTag[firstKey] = dict()
+                tagAfterTag[firstKey][secondKey] = times
         if whichDict == 3:
-            wordAndTag[firstKey] = dict()
-            wordAndTag[firstKey][secondKey] = times
-
+            if firstKey in wordAndTag:
+                wordAndTag[firstKey][secondKey] = times
+            else:
+                wordAndTag[firstKey] = dict()
+                wordAndTag[firstKey][secondKey] = times
     return tagAppearTime, tagBeforeTag, tagAfterTag, wordAndTag
 
 def viterbi(words, tagAppearTime, tagBeforeTag, tagAfterTag, wordAndTag, smoothing = False):
-    localTagAppearTime = tagAppearTime.copy()
-    localTagBeforeTag = tagBeforeTag.copy()
-    localTagAfterTag = tagAfterTag.copy()
-    localWordAndTag = wordAndTag.copy()
+    localTagAppearTime = deepcopy(tagAppearTime)
+    localTagBeforeTag = deepcopy(tagBeforeTag)
+    localTagAfterTag = deepcopy(tagAfterTag)
+    localWordAndTag = deepcopy(wordAndTag)
     localTagBeforeTag["</s>"] = dict()
     localTagAfterTag["<s>"] = dict()
     tag = list()
-    for word in words:
-        if word in localWordAndTag:
-            for key in localTagAppearTime.keys():
-                if key in localWordAndTag[word]:
-                    localWordAndTag[word][key] += 1
-                    localTagAppearTime[key] += 1
-                else:
-                    localWordAndTag[word][key] = 1
-                    localTagAppearTime[key] += 1
-        else:
-            localWordAndTag[word] = dict()
-            for key in localTagAppearTime.keys():
-                localTagAppearTime[key] += 1
-                localWordAndTag[word][key] = 1
-
     numOfWords = len(words)
     Tags = list(localTagAppearTime.keys())
     numOfTags = len(localTagAppearTime.keys())
-    table = [[0 for i in range(numOfTags)] for j in range(numOfWords)]
+    table = np.zeros((numOfWords, numOfTags))
     backpoint = [[-1 for i in range(numOfTags)] for j in range(numOfWords)]
     tempMax = 0
     argMax = -1
     if not smoothing:
         for i in range(numOfTags):
-            if Tags[i] in localTagBeforeTag["<s>"]:
-                table[0][i] = (localTagBeforeTag["<s>"][Tags[i]] / localTagAppearTime["<s>"]) * (localWordAndTag[words[0]][Tags[i]] / localTagAppearTime[Tags[i]])
+            if words[0] in localWordAndTag.keys():
+                if Tags[i] in localTagBeforeTag["<s>"] and Tags[i] in localWordAndTag[words[0]]:
+                    table[0][i] = (localTagBeforeTag["<s>"][Tags[i]] / localTagAppearTime["<s>"]) * (localWordAndTag[words[0]][Tags[i]] / localTagAppearTime[Tags[i]])
             else:
-                table[0][i] = 0
+                if Tags[i] in localTagBeforeTag["<s>"]:
+                    table[0][i] = (localTagBeforeTag["<s>"][Tags[i]] / localTagAppearTime["<s>"])
         for i in range(1, numOfWords):
             for j in range(numOfTags):
                 for k in range(numOfTags):
-                    if Tags[j] in localTagBeforeTag[Tags[k]]:
-                        cal = (localTagBeforeTag[Tags[k]][Tags[j]] / localTagAppearTime[Tags[k]]) * (localWordAndTag[words[i]][Tags[j]] / localTagAppearTime[Tags[j]]) * table[i-1][k]
-                        if  cal > table[i][j]:
-                            table[i][j] = cal
-                            backpoint[i][j] = k
+                    if words[i] in localWordAndTag.keys():
+                        if Tags[j] in localTagBeforeTag[Tags[k]] and Tags[j] in localWordAndTag[words[i]]: 
+                            cal = (localTagBeforeTag[Tags[k]][Tags[j]] / localTagAppearTime[Tags[k]]) * (localWordAndTag[words[i]][Tags[j]] / localTagAppearTime[Tags[j]]) * table[i-1][k]
+                            if cal > table[i][j]:
+                                table[i][j] = cal
+                                backpoint[i][j] = k
                     else:
-                        table[i][j] = 0
+                        if Tags[j] in localTagBeforeTag[Tags[k]]:
+                            cal = (localTagBeforeTag[Tags[k]][Tags[j]] / localTagAppearTime[Tags[k]]) * table[i-1][k]
+                            if cal > table[i][j]:
+                                table[i][j] = cal
+                                backpoint[i][j] = k
         for i in range(numOfTags):
             if "</s>" in localTagBeforeTag[Tags[i]]:
                 cal = (localTagBeforeTag[Tags[i]]["</s>"] / localTagAppearTime[Tags[i]]) * table[numOfWords-1][i]
                 if cal > tempMax:
                     tempMax = cal
                     argMax = i
-    else:
-        for i in range(numOfTags):
-            if Tags[i] in localTagBeforeTag["<s>"]:
-                table[0][i] = ((localTagBeforeTag["<s>"][Tags[i]] + 1) / (localTagAppearTime["<s>"] + numOfTags)) * (localWordAndTag[words[0]][Tags[i]] / localTagAppearTime[Tags[i]]) 
-            else:
-                table[0][i] = ( 1 / (localTagAppearTime["<s>"] + numOfTags)) * (localWordAndTag[words[0]][Tags[i]] / localTagAppearTime[Tags[i]])
-        for i in range(1, numOfWords):
-            for j in range(numOfTags):
-                for k in range(numOfTags):
-                    if Tags[j] in localTagBeforeTag[Tags[k]]:
-                        cal = ((localTagBeforeTag[Tags[k]][Tags[j]] + 1) / (localTagAppearTime[Tags[k]] + numOfTags)) * (localWordAndTag[words[i]][Tags[j]] / localTagAppearTime[Tags[j]]) * table[i-1][k]
-                    else:
-                        cal = ( 1 / (localTagAppearTime[Tags[k]] + numOfTags)) * (localWordAndTag[words[i]][Tags[j]] / localTagAppearTime[Tags[j]]) * table[i-1][k]
-                    if  cal > table[i][j]:
-                        table[i][j] = cal
-                        backpoint[i][j] = k
-        for i in range(numOfTags):
-            if "</s>" in localTagBeforeTag[Tags[i]]:
-                cal = ((localTagBeforeTag[Tags[i]]["</s>"] + 1) / (localTagAppearTime[Tags[i]] + numOfTags)) * table[numOfWords-1][i]
-            else:
-                cal = ( 1 / (localTagAppearTime[Tags[i]] + numOfTags)) * table[numOfWords-1][i]
-            if cal > tempMax:
-                tempMax = cal
-                argMax = i
         
     for i in range(numOfWords-1, -1, -1):
         tag.append(Tags[argMax])
         argMax = backpoint[i][argMax]
     tag = tag[::-1]
-    assert(len(tag) == len(words))      
+    assert(len(tag) == len(words))
     return tag
 
 def tag_sentence(test_file, model_file, out_file):
--- NLP/POS-tagging/test.py
+++ NLP/POS-tagging/test.py
@@ -1,8 +1,3 @@
-def f(a):
-    b = list(a.keys())
-    print(type(b))
-    print(b)
-a = {"1":2, "2":3, "3":4}
-print(a)
-f(a)
+import numpy as np
+a = np.zeros((3, 4))
 print(a)
